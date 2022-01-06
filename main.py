ef45d7c78431fe0bf371b5a6986ef2feee3a1c8a
Serafim-uliuliuc <serafim.uliuliuc@info.uaic.ro>
2022-01-06 21:39:54 +0200

    Functie run (pentru worker)

--- main.py
+++ main.py
@@ -1,12 +1,17 @@
-import json, requests, multiprocessing, rq
+import json, requests, multiprocessing, pika
 from bs4 import BeautifulSoup
 
 urlBaza = 'https://www.alexa.com/topsites/countries'
 urlSit = 'https://www.'
+cale = 'E:\\info\\PP\\Date\\'
 
 raspuns = requests.get(urlBaza)
 parseaza = BeautifulSoup(raspuns.text, 'html.parser')
 listaTaguri = parseaza.select('li a')
+
+# conexiune = pika.BlockingConnection(pika.ConnectionParameters('localhost'))
+# coada = conexiune.channel()
+# coada.queue_declare(queue='principal')
 
 listaTari = []
 for tag in listaTaguri:
@@ -15,12 +20,22 @@ for tag in listaTaguri:
 for tara in listaTari:
 
     if tara == '/AX':
-        continue #serverul din insulele aland nu raspunde
+        continue  # serverul din insulele aland nu raspunde
 
-    paginaTara = requests.get(urlBaza+tara)
+    paginaTara = requests.get(urlBaza + tara)
     parseaza = BeautifulSoup(paginaTara.text, 'html.parser')
 
     listaSituri = parseaza.select('div > p > a')
-    print(tara)
+
+    adresa = cale + tara[1:]   # print(tara)
+    dictionar = {}
+
     for sit in listaSituri:
-        print(urlSit+sit.get('href').removeprefix('/siteinfo/'))
+        dictionar['web'] = (urlSit + sit.get('href').removeprefix('/siteinfo/'))
+        dictionar['local'] = adresa
+
+        dePusInCoada=json.dumps(dictionar)
+        # coada.basic_publish(exchange='',routing_key='principal',body=dePusInCoada)
+        print(dePusInCoada)
+
+# conexiune.close()
---
+++ worker.py
@@ -0,0 +1,14 @@
+import pika, json, requests
+
+conexiune = pika.BlockingConnection(pika.ConnectionParameters('localhost'))
+coada = conexiune.channel()
+coada.queue_declare(queue='principal')
+
+def run(ch, method, properties, body):
+    date = json.load(body)
+    fisier = open(date['local']+date['web'].removeprefix('https://www.'), 'a')
+    pagina = requests.get(date['web'])
+    fisier.write(pagina.text)
+    fisier.close()
+
+conexiune.close()
