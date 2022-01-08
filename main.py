e557eafc840891bef06e14c26df3f7b1b56b6b05
Serafim-uliuliuc <serafim.uliuliuc@info.uaic.ro>
2022-01-08 16:50:36 +0200

    logging

--- main.py
+++ main.py
@@ -1,18 +1,24 @@
-import json, requests, multiprocessing, pika
+import json
+import logging
+import pika
+import requests
+import subprocess
+import sys
+
 from bs4 import BeautifulSoup
-import subprocess, sys
 
 urlBaza = 'https://www.alexa.com/topsites/countries'
 urlSit = 'https://www.'
 cale = 'E:\\info\\PP\\Date\\'
+logging.basicConfig(filename='master.log', filemode='w', format='%(asctime) - %(name): %(message)')
 
 raspuns = requests.get(urlBaza)
 parseaza = BeautifulSoup(raspuns.text, 'html.parser')
 listaTaguri = parseaza.select('li a')
 
-# conexiune = pika.BlockingConnection(pika.ConnectionParameters('localhost'))
-# coada = conexiune.channel()
-# coada.queue_declare(queue='principal')
+conexiune = pika.BlockingConnection(pika.ConnectionParameters('localhost'))
+coada = conexiune.channel()
+coada.queue_declare(queue='principal')
 
 listaTari = []
 for tag in listaTaguri:
@@ -20,25 +26,27 @@ for tag in listaTaguri:
 
 for tara in listaTari:
 
-    if tara == '/AX':
-        continue  # serverul din insulele aland nu raspunde
+    paginaTara = requests.get(urlBaza + tara, timeout=5)
 
-    paginaTara = requests.get(urlBaza + tara)
-    parseaza = BeautifulSoup(paginaTara.text, 'html.parser')
+    if paginaTara.ok:
+        parseaza = BeautifulSoup(paginaTara.text, 'html.parser')
 
-    listaSituri = parseaza.select('div > p > a')
+        listaSituri = parseaza.select('div > p > a')
 
-    adresa = cale + tara[1:]   # print(tara)
-    dictionar = {}
+        adresa = cale + tara[1:]  # print(tara)
+        dictionar = {}
 
-    for sit in listaSituri:
-        dictionar['web'] = (urlSit + sit.get('href').removeprefix('/siteinfo/'))
-        dictionar['local'] = adresa
+        for sit in listaSituri:
+            dictionar['web'] = (urlSit + sit.get('href').removeprefix('/siteinfo/'))
+            dictionar['local'] = adresa
 
-        dePusInCoada=json.dumps(dictionar)
-        # coada.basic_publish(exchange='',routing_key='principal',body=dePusInCoada)
-        print(dePusInCoada)
+            dePusInCoada = json.dumps(dictionar)
+            coada.basic_publish(exchange='', routing_key='principal', body=dePusInCoada)
 
+        logging.info('Am adaugat in coada siturile de la tara: ' + tara)
+    else:
+        mesajEroare = 'Eroare la tara: ' + tara
+        logging.info(mesajEroare)
 
 procese = []
 for proces in range(len(listaTari)):
@@ -47,4 +55,4 @@ for proces in range(len(listaTari)):
 for proces in procese:
     proces.wait()
 
-# conexiune.close()
+conexiune.close()
--- worker.py
+++ worker.py
@@ -1,17 +1,30 @@
-import pika, json, requests
+import logging
 
+import json
+import pika
+import requests
+
+logging.basicConfig(filename='slave.log', filemode='w', format='%(asctime) - %(name): %(message)')
 conexiune = pika.BlockingConnection(pika.ConnectionParameters('localhost'))
 coada = conexiune.channel()
 coada.queue_declare(queue='principal')
+coada.basic_qos(prefetch_count=1)
+
 
 def run(ch, method, properties, body):
     date = json.load(body)
-    fisier = open(date['local']+date['web'].removeprefix('https://www.'), 'a')
-    pagina = requests.get(date['web'])
-    fisier.write(pagina.text)
+    fisier = open(date['local'] + date['web'].removeprefix('https://www.'), 'a')
+    pagina = requests.get(date['web'], timeout=5)
+    if pagina.ok:
+        fisier.write(pagina.text)
+        logging.info('Situl ' + date['web'] + ' pentru tara ' + date['local'][-2:]) + 'a fost descaract cu succes!'
+    else:
+        mesaj = 'Eroare la descarcare ' + date['web'] + ' pentru tara ' + date['local'][-2:]
+        logging.info(mesaj)
     fisier.close()
 
-for i in range(50):
 
+for i in range(50):
+    coada.basic_consume(queue='principal', on_message_callback=run)
 
 conexiune.close()
